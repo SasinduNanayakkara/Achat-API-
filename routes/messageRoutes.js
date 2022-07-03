61f35bc5030b0c3296206f882b17534d867df8bb
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-04 02:36:12 +0530

    Define API routes

---
+++ routes/messageRoutes.js
@@ -0,0 +1,8 @@
+const { addMessage, getMessages } = require('../controllers/messageController');
+
+const router = require('express').Router();
+
+router.post("/addmsg", addMessage);
+router.get("/getmsg/", getMessages);
+
+module.exports = router;
