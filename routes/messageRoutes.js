5060f20cf2055070a387b46747e3a8284801faa8
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-04 16:07:27 +0530

    Chnage route pattern

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -3,6 +3,6 @@ const { addMessage, getMessages } = require('../controllers/messageController');
 const router = require('express').Router();
 
 router.post("/addmsg", addMessage);
-router.get("/getmsg/", getMessages);
+router.post("/getmsg/", getMessages);
 
 module.exports = router;
